627511a69cfd87b102891a946889449f1a02619c
gersoncorrea <gersoncorrea.jr@gmail.com>
2023-01-28 23:59:18 -0400

    [JC1.2] - aula 3 composable com estado

--- app/src/main/java/com/study/alura_jetpackcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/study/alura_jetpackcompose/ui/screens/HomeScreen.kt
@@ -1,16 +1,19 @@
 package com.study.alura_jetpackcompose.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.study.alura_jetpackcompose.R
 import com.study.alura_jetpackcompose.model.Product
 import com.study.alura_jetpackcompose.sampledata.sampleProducts
 import com.study.alura_jetpackcompose.ui.components.ProductSection
@@ -18,25 +21,43 @@ import java.math.BigDecimal
 
 @Composable
 fun HomeScreen() {
-    Column(
-        Modifier.fillMaxSize()
-            .verticalScroll(rememberScrollState()),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
+    Column {
+        val text = remember { mutableStateOf("") }
+        OutlinedTextField(value = text.value, onValueChange = { newValue ->
+            text.value = newValue
+            Log.i("HomeScreen", "HomeScreen: TextField $newValue")
+            Log.i("HomeScreen", "HomeScreen: TextField ${text.value}")
+        })
         Spacer(modifier = Modifier)
-        ProductSection("Promoções", sampleProducts)
-        ProductSection(
-            "Doces",
-            listOf(
-                Product(
-                    name = "Chocolate",
-                    price = BigDecimal("5.99"),
-                    image = "https://images.pexels.com/photos/65882/chocolate-dark-coffee-confiserie-65882.jpeg"
+
+        LazyColumn(
+            Modifier.fillMaxSize().padding(top = 40.dp),
+
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier)
+                ProductSection("Promoções", sampleProducts)
+            }
+
+            item {
+                ProductSection(
+                    "Doces",
+                    listOf(
+                        Product(
+                            name = "Chocolate",
+                            price = BigDecimal("5.99"),
+                            image = "https://images.pexels.com/photos/65882/chocolate-dark-coffee-confiserie-65882.jpeg"
+                        )
+                    )
                 )
-            )
-        )
-        ProductSection("Bebidas", sampleProducts)
-        Spacer(modifier = Modifier)
+            }
+
+            item {
+                ProductSection("Bebidas", sampleProducts)
+                Spacer(modifier = Modifier)
+            }
+        }
     }
 }
 
