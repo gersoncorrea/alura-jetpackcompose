40ecfd478d2f4b7ac9c0486d47145793fab95c89
gersoncorrea <gersoncorrea.jr@gmail.com>
2023-01-29 00:06:58 -0400

    [JC1.2] - aula 3 personalizando slot api

--- app/src/main/java/com/study/alura_jetpackcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/study/alura_jetpackcompose/ui/screens/HomeScreen.kt
@@ -1,13 +1,18 @@
 package com.study.alura_jetpackcompose.ui.screens
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
 import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -23,11 +28,21 @@ import java.math.BigDecimal
 fun HomeScreen() {
     Column {
         val text = remember { mutableStateOf("") }
-        OutlinedTextField(value = text.value, onValueChange = { newValue ->
-            text.value = newValue
-            Log.i("HomeScreen", "HomeScreen: TextField $newValue")
-            Log.i("HomeScreen", "HomeScreen: TextField ${text.value}")
-        })
+        OutlinedTextField(
+            value = text.value,
+            onValueChange = { newValue ->
+                text.value = newValue
+            },
+            Modifier.padding(start = 16.dp, top = 16.dp, end = 16.dp).fillMaxWidth(),
+            shape = RoundedCornerShape(25),
+            leadingIcon = { Icon(Icons.Default.Search, contentDescription = "ícone de lupa") },
+            label = {
+                Text(text = "Produto")
+            },
+            placeholder = {
+                Text(text = "O que você procura?")
+            }
+        )
         Spacer(modifier = Modifier)
 
         LazyColumn(
